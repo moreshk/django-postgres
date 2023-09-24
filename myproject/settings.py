1e20732712f2b974c22579d9b146d895508b6bf7
moreshk <moreshk@gmail.com>
2023-09-24 12:30:38 +1000

    changed debug to environment variable

--- myproject/settings.py
+++ myproject/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8b=5^&424wlmi=^g1dcdt1zf2k=6t(#8aakdhjewn*2_9480jb"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('ENV_DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
