2f9e4f58a6c368cf023069ec6d47e388a5bf2fa3
moreshk <moreshk@gmail.com>
2023-10-01 23:17:33 +1100

    generate test using open ai api call and langchain

--- essay_grader_app/views.py
+++ essay_grader_app/views.py
@@ -1,12 +1,11 @@
 # Create your views here.
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .generator import generate_test  # Import the function
-
+from .generator import generate_test_data  # Import the function
 
 @login_required
 def index(request):
     message = ""
     if request.method == "POST" and "generate_test" in request.POST:
-        message = generate_test()
+        message = generate_test_data()
     return render(request, 'essay_grader_app/index.html', {'message': message})
