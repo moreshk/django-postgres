828497931165e488548775f2838475954e4b346d
moreshk <moreshk@gmail.com>
2023-10-01 22:40:24 +1100

    test creator function call demo

---
+++ essay_grader_app/generator.py
@@ -0,0 +1,4 @@
+# essay_grader_app/generator.py
+
+def generate_test():
+    return "Hello World"
--- essay_grader_app/views.py
+++ essay_grader_app/views.py
@@ -1,7 +1,12 @@
 # Create your views here.
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .generator import generate_test  # Import the function
+
 
 @login_required
 def index(request):
-    return render(request, 'essay_grader_app/index.html')
+    message = ""
+    if request.method == "POST" and "generate_test" in request.POST:
+        message = generate_test()
+    return render(request, 'essay_grader_app/index.html', {'message': message})
