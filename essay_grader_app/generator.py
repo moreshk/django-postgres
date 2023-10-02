f59a93a6348baeba0721bf29f2b6128f2331a0e3
moreshk <moreshk@gmail.com>
2023-10-02 12:19:30 +1100

    essay grading response

--- essay_grader_app/generator.py
+++ essay_grader_app/generator.py
@@ -94,3 +94,37 @@ def generate_test_data():
         return f"Title: {title}\nDescription: {description}"
     else:
         return "Error: Unexpected data format"
+
+
+def grade_essay(user_response, title, description):
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    prompt = PromptTemplate(
+        input_variables=["essay", "task_title", "task_desc"],
+        template="""You are an essay grader for IELTS Writing Task 2. Your criteria for grading is how well the provided essay fulfils the requirements of the task, 
+        including whether it has addressed all parts of the task description and provided a clear position. 
+        The key points for your grading are:
+        1. Does the essay address all parts of the prompt (task and task description).
+        2. Provides a clear thesis statement that outlines the writers position.
+        3. Support the writers arguments with relevant examples and evidence.
+        
+        Task Title: {task_title}
+
+        Task Description: {task_desc}
+
+        Essay: {essay}
+
+        Grade the essay out of 10 on each of the 3 points. Provide detailed description about how much you graded the essay on each of the points and provide feedback on how it could improve. Finally provide the average grade based on the 3 grades.
+        """,
+    )
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    inputs = {
+        "essay": user_response,
+        "task_title": title,
+        "task_desc": description
+    }
+
+    feedback_from_api = chain.run(inputs)
+    return feedback_from_api
--- essay_grader_app/views.py
+++ essay_grader_app/views.py
@@ -1,11 +1,22 @@
 # Create your views here.
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .generator import generate_test_data  # Import the function
+from .generator import generate_test_data, grade_essay
 
 @login_required
 def index(request):
     message = ""
-    if request.method == "POST" and "generate_test" in request.POST:
-        message = generate_test_data()
-    return render(request, 'essay_grader_app/index.html', {'message': message})
+    feedback = ""
+    if request.method == "POST":
+        if "generate_test" in request.POST:
+            message = generate_test_data()
+            # Assuming the message format is "Title: xxx\nDescription: yyy"
+            title, description = message.split("\n")
+            request.session['task_title'] = title.split(":")[1].strip()
+            request.session['task_description'] = description.split(":")[1].strip()
+        elif "text_input" in request.POST:
+            user_response = request.POST.get('text_input')
+            title = request.session.get('task_title', 'Default Title')  # You can fetch this from the session or database
+            description = request.session.get('task_description', 'Default Description')  # You can fetch this from the session or database
+            feedback = grade_essay(user_response, title, description)
+    return render(request, 'essay_grader_app/index.html', {'message': message, 'feedback': feedback})
