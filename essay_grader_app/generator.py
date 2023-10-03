63f948288f70bf49d6aefc6cb6153b40c6fe7fa4
moreshk <moreshk@gmail.com>
2023-10-03 16:21:13 +1100

    param

--- essay_grader_app/generator.py
+++ essay_grader_app/generator.py
@@ -22,9 +22,12 @@ def generate_test_data(exam_type, essay_type, grade):
 
 
     # This is an LLMChain to create a task for a particular exam
+    print(exam_type, essay_type,grade)
 
     llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
-    template = """You are a test task creator for {test_type}. I will provide you the test type and your job is to create a task for a test taker.
+
+
+    ielts_template = """You are a test task creator for {test_type}. I will provide you the test type and your job is to create a task for a test taker.
 
     You will provide a topic and ask the user if they agree or disagree, provide pros and cons etc and provide their views on the topic.
 
@@ -57,6 +60,88 @@ def generate_test_data(exam_type, essay_type, grade):
 
     Create 5 such tasks and descriptions based on the above guidelines. In your output mention these ten tasks and format the output as Title:  and Description: .
     """
+
+    naplan_narrative_template = """You are a test task creator for {test_type}. I will provide you the test type and your job is to create a task for a test taker.
+
+    You will provide a topic and ask the user if they agree or disagree, provide pros and cons etc and provide their views on the topic.
+
+    Sample Tasks for {test_type}
+    Sample 1: Technology
+    Task: "Some people believe that technological advancements lead to the loss of traditional cultures. To what extent do you agree or disagree?"
+
+    Sample 2: Education
+    Task: "Some educators argue that every child should be taught how to play a musical instrument. Discuss the advantages and disadvantages of this. Give your own opinion."
+
+    Sample 3: Environment
+    Task: "Climate change is now an accepted threat to our planet, but there is not enough political action to control excessive consumerism and pollution. Discuss both views and give your own opinion."
+
+    Sample 4: Health
+    Task: "Some people think that governments should focus on reducing healthcare costs, rather than funding arts and sports. Do you agree or disagree?"
+
+    Sample 5: Society
+    Task: "Some people think that the best way to reduce crime is to give longer prison sentences. Others, however, believe there are better alternative ways of reducing crime. Discuss both views and give your opinion."
+
+    Sample 6: Work
+    Task: "Remote work is becoming increasingly popular. Discuss the advantages and disadvantages of working from home."
+
+    Sample 7: Global Issues
+    Task: "Some people argue that developed countries have a higher obligation to combat climate change than developing countries. Discuss both sides and give your own opinion."
+
+    Sample 8: Science
+    Task: "Genetic engineering is an important issue in modern society. Some people think that it will improve people’s lives in many ways. Others feel that it may be a threat to life on earth. Discuss both these views and give your own opinion."
+
+    You will use the above examples only as a guideline for framing the task and create a new task and description randomly on a different topic. No need to use the word Sample in the task description. 
+
+    Create 5 such tasks and descriptions based on the above guidelines. In your output mention these ten tasks and format the output as Title:  and Description: .
+    """
+
+    naplan_pesuasive_template = """You are a test task creator for {test_type}. I will provide you the test type and your job is to create a task for a test taker.
+
+    You will provide a topic and ask the user if they agree or disagree, provide pros and cons etc and provide their views on the topic.
+
+    Sample Tasks for {test_type}
+    Sample 1: Technology
+    Task: "Some people believe that technological advancements lead to the loss of traditional cultures. To what extent do you agree or disagree?"
+
+    Sample 2: Education
+    Task: "Some educators argue that every child should be taught how to play a musical instrument. Discuss the advantages and disadvantages of this. Give your own opinion."
+
+    Sample 3: Environment
+    Task: "Climate change is now an accepted threat to our planet, but there is not enough political action to control excessive consumerism and pollution. Discuss both views and give your own opinion."
+
+    Sample 4: Health
+    Task: "Some people think that governments should focus on reducing healthcare costs, rather than funding arts and sports. Do you agree or disagree?"
+
+    Sample 5: Society
+    Task: "Some people think that the best way to reduce crime is to give longer prison sentences. Others, however, believe there are better alternative ways of reducing crime. Discuss both views and give your opinion."
+
+    Sample 6: Work
+    Task: "Remote work is becoming increasingly popular. Discuss the advantages and disadvantages of working from home."
+
+    Sample 7: Global Issues
+    Task: "Some people argue that developed countries have a higher obligation to combat climate change than developing countries. Discuss both sides and give your own opinion."
+
+    Sample 8: Science
+    Task: "Genetic engineering is an important issue in modern society. Some people think that it will improve people’s lives in many ways. Others feel that it may be a threat to life on earth. Discuss both these views and give your own opinion."
+
+    You will use the above examples only as a guideline for framing the task and create a new task and description randomly on a different topic. No need to use the word Sample in the task description. 
+
+    Create 5 such tasks and descriptions based on the above guidelines. In your output mention these ten tasks and format the output as Title:  and Description: .
+    """
+
+
+    # Select the appropriate template based on exam_type and essay_type
+    if exam_type == "IELTS":
+        template = ielts_template
+    elif exam_type == "NAPLAN" and essay_type == "Narrative":
+        template = naplan_narrative_template
+    elif exam_type == "NAPLAN" and essay_type == "Persuasive":
+        template = naplan_pesuasive_template
+    else:
+        # Default to IELTS template (or any other default you prefer)
+        template = ielts_template
+        
+
     prompt_template = PromptTemplate(input_variables=["test_type"], template=template)
     task_creator_chain = LLMChain(llm=llm, prompt=prompt_template)
 
