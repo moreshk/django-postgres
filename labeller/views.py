e0ed2eed59887c956fdf5cc94f929634bc5c6137
moreshk <moreshk@gmail.com>
2023-11-12 23:35:49 +1100

    access control

--- labeller/views.py
+++ labeller/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 import yfinance as yf
 import os
-import random
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from datetime import datetime, timedelta
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
+@login_required
 def fetch_data():
     ticker = os.getenv('TICKER', '^GSPC')
     days = os.getenv('DAYS', '7')
@@ -17,8 +19,12 @@ def fetch_data():
     
     return data.reset_index(drop=True).to_dict('records')  # Drop the original index
 
-
+@login_required
 def candlestick_view(request):
+    if request.user.user_type != 'LABELLER':
+        messages.error(request, 'You do not have permission to access this page.')
+        return redirect('home')
+    
     if 'data' not in request.session:
         request.session['data'] = fetch_data()
         request.session['index'] = 0
@@ -42,11 +48,13 @@ def candlestick_view(request):
         'total': len(request.session['data']),
     })
 
+@login_required
 def back_view(request):
     if request.session['index'] > 0:
         request.session['index'] -= 1
     return redirect('candlestick_view')
 
+@login_required
 def forward_view(request):
     if request.session['index'] < len(request.session['data']) - 1:
         request.session['index'] += 1
