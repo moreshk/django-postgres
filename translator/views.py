f3ff28a0f1552757e73bfc87320d637f0a5cc910
Moresh Kokane <moreshkokane@Moreshs-MacBook-Pro.local>
2023-12-06 09:13:03 +0530

    taking generated sentence from session to prevent its regeneration

--- translator/views.py
+++ translator/views.py
@@ -12,28 +12,31 @@ from .models import Translation
 
 @login_required
 def random_sentence(request, topic):
-    print("Topic: ", topic)
-    llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
-    template = f"Generate a random sentence with respect to the following: {{input_param}}."
-    prompt_template = PromptTemplate(input_variables=["input_param"], template=template)
-    chain = LLMChain(llm=llm, prompt=prompt_template)
-    result = chain.run({"input_param": topic})
-    
-    sentence = result
-
     if request.method == 'POST':
         form = TranslationForm(request.POST)
         if form.is_valid():
             translation = form.save(commit=False)
             translation.user = request.user
+            # Retrieve the sentence from the session instead of generating a new one
+            sentence = request.session.get('sentence', '')
             translation.english_text = sentence
             translation.status = 'Submitted'
             translation.save()
             return redirect('random_sentence', topic=topic)
     else:
+        llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
+        template = f"Generate a random sentence with respect to the following: {{input_param}}."
+        prompt_template = PromptTemplate(input_variables=["input_param"], template=template)
+        chain = LLMChain(llm=llm, prompt=prompt_template)
+        result = chain.run({"input_param": topic})
+        
+        sentence = result
+        # Store the generated sentence in the session
+        request.session['sentence'] = sentence
         form = TranslationForm(initial={'translated_language': request.user.language})
 
     return render(request, 'translator/random_sentence.html', {'sentence': sentence, 'form': form, 'topic': topic})
+
 
 @login_required
 def user_translations(request):
