949fdc6eb9e70a6e3c64c5c8b5ce6a468495c0a7
Moresh Kokane <moreshkokane@Moreshs-MacBook-Pro.local>
2023-12-12 17:07:31 +0530

    added finetuning dataset and changed home page to show courses to all user types

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-import assemblyai as aai
-
-aai.settings.api_key = "3d91e1f391bf4e15b4ed43e9831d4d77"
-
-audio_url = "https://github.com/AssemblyAI-Examples/audio-examples/raw/main/20230607_me_canadian_wildfires.mp3"
-
-transcriber = aai.Transcriber()
-
-transcript = transcriber.transcribe(audio_url)
-print(transcript.export_subtitles_srt())
+from dotenv import load_dotenv
+import os
+load_dotenv()
+print(os.environ.get('ELEVENLABS_API_KEY'))  # This should print the port number
